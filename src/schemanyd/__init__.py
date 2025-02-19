938dcad262adb0cdbddab39e63c720507767ad54
t3llscode <git@t3l.ls>
2025-02-19 20:21:24 +0100

    chore: establish src structure frame

---
+++ src/schemanyd/__init__.py
@@ -0,0 +1,13 @@
+"""
+schemanyd
+
+Revolutionizing database population.
+"""
+
+# from XXX import Y, Z, ...
+
+__version__ = "0.1.0"
+__author__ = "Tell Hensel"
+__email__ = "git@t3l.ls"
+
+__all__ = []
